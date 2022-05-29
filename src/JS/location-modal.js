71031d628beba4dd6bf9c2fbc9fb1c75949db640
Anton <anton_surkov@rambler.ru>
2022-05-29 12:17:13 +0300

    fix

---
+++ src/JS/location-modal.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[loc-data-modal-open]'),
+    closeModalBtn: document.querySelector('[loc-data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('loc-modal-open');
+    refs.modal.classList.toggle('loc-is-hidden');
+  }
+})();
