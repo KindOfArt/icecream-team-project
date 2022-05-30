30f06de03d68bd69ebf4cc771c3b3e3537fe63ef
Anton <anton_surkov@rambler.ru>
2022-05-30 14:33:32 +0300

    debugged franshize

--- src/JS/modal-franchise.js
+++ src/JS/modal-franchise.js
@@ -1,30 +1,24 @@
 // Get the modal
-var modal = document.getElementById('modalFranchise');
+const modal = document.getElementById('modalFranchise');
 
 // Get the button that opens the modal
-var btn = document.getElementById('btnFranchise');
+const btn = document.getElementById('btnFranchise');
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName('franchise__close')[0];
-var btnFrsbm = document.getElementsByClassName('button__franchise-submit')[0];
+const span = document.querySelector('.franchise__close');
+const btnFrsbm = document.querySelector('.button__franchise-submit');
 
 // When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = 'block';
-};
+btn.addEventListener('click', () => (modal.style.display = 'block'));
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = 'none';
-};
+span.addEventListener('click', () => (modal.style.display = 'none'));
 // When the user clicks on <button__franchise-submit>, close the modal
-btnFrsbm.onclick = function () {
-  modal.style.display = 'none';
-};
+btnFrsbm.addEventListener('click', () => (modal.style.display = 'none'));
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
+window.addEventListener('click', event => {
+  if (event.target === modal) {
     modal.style.display = 'none';
   }
-};
+});
