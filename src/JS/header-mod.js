5dee7b200f18f136310cd02d8740489c2e0ccade
Mariia Yavorska <m.litvinenko16@gmail.com>
2022-05-29 22:10:07 -0400

    add  modal open to mobile menu button

--- src/JS/header-mod.js
+++ src/JS/header-mod.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[header-data-modal-open]'),
+    openModalMenuBtn: document.querySelector('[menu-data-modal-form-open]'),
     closeModalBtn: document.querySelector('[header-data-modal-close]'),
     modal: document.querySelector('[header-data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalMenuBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
