6aeb39409d1401f69748522ca9845cf977bb5f3e
Anton <anton_surkov@rambler.ru>
2022-06-01 12:09:25 +0300

    fix

--- src/JS/mob-menu.js
+++ src/JS/mob-menu.js
@@ -2,22 +2,24 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  const menuList = document.querySelector('.menu__list');
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
   };
-
+  const removeMenu = () => {
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', true);
+  };
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
+  menuList.addEventListener('click', removeMenu);
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 1280px)').addEventListener('change', e => {
