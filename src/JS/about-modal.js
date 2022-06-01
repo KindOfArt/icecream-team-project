b78145281d15d4036a542283a3049f4997e27ed9
kovalenkoandri <kovalenkoandrii@gmail.com>
2022-06-01 11:53:26 +0300

    animation

--- src/JS/about-modal.js
+++ src/JS/about-modal.js
@@ -1,22 +1,14 @@
 (() => {
   const refs = {
     openModalBtnAbout: document.querySelector('[data-modal-open-about]'),
-    closeModalBtnAbout: document.querySelector('[data-modal-close-about]'),
     modalAbout: document.querySelector('[data-modal-about]'),
   };
 
   refs.openModalBtnAbout.addEventListener('click', toggleModalAbout);
-  refs.closeModalBtnAbout.addEventListener('click', toggleModalAbout);
+  refs.modalAbout.addEventListener('click', toggleModalAbout);
 
   function toggleModalAbout() {
     document.body.classList.toggle('about-read-more');
     refs.modalAbout.classList.toggle('about-is-hidden');
   }
-  // on click anywhere NOT the window close modal
-  window.addEventListener('click', event => {
-    if (event.target === refs.modalAbout) {
-      refs.modalAbout.style.display = 'none';
-      toggleModalAbout();
-    } 
-  });
 })();
--- src/JS/franchise-modal.js
+++ src/JS/franchise-modal.js
@@ -12,11 +12,4 @@
     document.body.classList.toggle('franchise');
     refs.modalFranchise.classList.toggle('franchise-is-hidden');
   }
-  // on click anywhere NOT the window close modal
-  window.addEventListener('click', event => {
-    if (event.target === refs.modalFranchise) {
-      refs.modalFranchise.style.display = 'none';
-      toggleModalFranchise();
-    }
-  });
 })();
