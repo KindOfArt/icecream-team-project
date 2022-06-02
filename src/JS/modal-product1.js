db6b9c2ad2f2da063c8e02c58103288052ebc465
Anton <anton_surkov@rambler.ru>
2022-06-02 04:33:12 +0300

    cosmetical rework

--- src/JS/modal-product1.js
+++ src/JS/modal-product1.js
@@ -9,7 +9,7 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('lock');
-    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('lock');
   }
 })();
